6c5dad0649916bb5008673cbe57bf6bcb841f660
Miroslav Bergam <mlbergam@gmail.com>
2020-09-27 19:42:48 -0500

    added data

--- scripts/forecast2020.R
+++ scripts/forecast2020.R
@@ -3,15 +3,15 @@
 library(tidyverse)
 library(ggplot2)
 
-setwd("~/Google Drive Harvard/Courses/Gov1347-ElectionAnalysis-2020/Sections-Public/03-Polling")
+setwd("/Users/mirobergam/Desktop/Election-Analytics")
 
 #####------------------------------------------------------#
 ##### Read and merge data ####
 #####------------------------------------------------------#
 
-popvote_df <- read_csv("popvote_1948-2016.csv")
-economy_df <- read_csv("econ.csv")
-poll_df    <- read_csv("pollavg_1968-2016.csv")
+popvote_df <- read_csv("../data/popvote_1948-2016.csv")
+economy_df <- read_csv("../data/econ.csv")
+poll_df    <- read_csv("../data/pollavg_1968-2016.csv")
 
 dat <- popvote_df %>% 
     full_join(poll_df %>% 
